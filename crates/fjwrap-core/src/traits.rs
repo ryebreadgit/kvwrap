5cf2dd97fe5abee2d11602e71aedd797c48f20df
RyeBread <rye@rye.moe>
2026-01-28 20:57:22 -0700

    Add Arc implementations from dyn KvStore to fix Sized requirement issues

--- crates/fjwrap-core/src/traits.rs
+++ crates/fjwrap-core/src/traits.rs
@@ -1,6 +1,7 @@
 use crate::{Error, Result};
 use async_trait::async_trait;
 use serde::{Serialize, de::DeserializeOwned};
+use std::sync::Arc;
 
 #[async_trait]
 pub trait KvStore: Send + Sync {
@@ -26,5 +27,20 @@ pub trait KvStore: Send + Sync {
     {
         let bytes = serde_json::to_vec(value).map_err(Error::SerdeJson)?;
         self.set(partition, key, &bytes).await
+    }
+}
+
+#[async_trait]
+impl KvStore for Arc<dyn KvStore> {
+    async fn get(&self, partition: &[u8], key: &[u8]) -> Result<Option<Vec<u8>>> {
+        (**self).get(partition, key).await
+    }
+
+    async fn set(&self, partition: &[u8], key: &[u8], value: &[u8]) -> Result<()> {
+        (**self).set(partition, key, value).await
+    }
+
+    async fn delete(&self, partition: &[u8], key: &[u8]) -> Result<()> {
+        (**self).delete(partition, key).await
     }
 }
